942a4eb331d7a66663473ddf9d146e0573b14f89
saul <zhuliminl@gmail.com>
2019-08-21 23:33:55 +0800

    seed

--- script/seed.ts
+++ script/seed.ts
@@ -1,8 +1,14 @@
 import userSeed from './seed/user.json'
+import materialSeed from './seed/material.json'
 import { prisma } from '../generated/prisma-client'
 import { logger } from '../src/common/logger'
 
 async function main() {
+  // initUsers()
+  initMaterails()
+}
+
+function initUsers() {
   const users = userSeed['users'] || []
 
   users.forEach(async user => {
@@ -17,6 +23,20 @@ async function main() {
       logger.error(err)
     }
   })
+}
+
+function initMaterails() {
+  const materials = materialSeed['materials'] || []
+  materials.forEach(async material => {
+    try {
+      await prisma.createMaterial({
+        ...material
+      })
+    } catch (err) {
+      logger.error(err)
+    }
+  })
+
 }
 
 main()
